5d067a2cfefeacd8bad124f7ef151beade08115f
rlrenjith86-ux <rlrenjith86@gmail.com>
2026-08-30 18:01:07 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Smart Library Portal</h1>
+
+      <p>
+        Smart Library Portal is starting successfully.
+      </p>
+    </div>
+  );
+}
+
+export default App;
