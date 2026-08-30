81eaf02fee5443e20f54d6f37b6b46f1f2482b7d
rlrenjith86-ux <rlrenjith86@gmail.com>
2026-08-30 18:18:41 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
-    "/.netlify/functions",
+  baseURL: "/.netlify/functions",
 
   headers: {
     "Content-Type": "application/json"
