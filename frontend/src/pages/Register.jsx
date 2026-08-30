1d28aa3fdd85a66c5e786fc96e52baf9db0ef93a
rlrenjith86-ux <rlrenjith86@gmail.com>
2026-08-30 18:05:25 +0530

    Create Register.jsx

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+function Register() {
+  return (
+    <div className="page-container">
+      <h1>Create Account</h1>
+
+      <p>
+        Student and Teacher registration will be added next.
+      </p>
+    </div>
+  );
+}
+
+export default Register;
